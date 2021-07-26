b8c34e2342972dc7a87b1e8bdb13cc93bbdd24b8
CB-Creation <64854641+CB-Creation@users.noreply.github.com>
2021-07-26 09:04:02 +0530

    product data are now getting from backend

---
+++ backend/data.js
@@ -0,0 +1,42 @@
+const data={
+    products:[
+        {
+            id:'1',
+            name:'Bathisara',
+            category:'Incense sticks',
+            image:'/img/1_.png',
+            price:'Rs. 100/=',
+            counterInStock:10,
+            brand:'Dewsanda',
+            rating:4.5,
+            numReviews:10,
+            description:'Bathisara 100 sticks',
+        },
+        {
+            id:'2',
+            name:'Six O\'clock',
+            category:'Incense sticks',
+            image:'/img/2_.png',
+            price:'Rs. 20/=',
+            counterInStock:20,
+            brand:'Dewsanda',
+            rating:5,
+            numReviews:7,
+            description:'Bathisara Six O\'clock',
+        },
+        {
+            id:'3',
+            name:'Dewsanda',
+            category:'Incense sticks',
+            image:'/img/3_.png',
+            price:'Rs. 80/=',
+            counterInStock:0,
+            brand:'Dewsanda',
+            rating:5,
+            numReviews:120,
+            description:'Dewsanda Premium',
+        }
+    ]
+}
+
+export default data;
